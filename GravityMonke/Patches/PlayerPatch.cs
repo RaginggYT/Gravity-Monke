e9e7b8ea42ce818785863f7396e8203f453ab060
RaginggYT <100000931+RaginggYT@users.noreply.github.com>
2022-04-17 13:44:03 -0400

    Update Source to v1.2.0 (Accessibility Update)

---
+++ GravityMonke/Patches/PlayerPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace GravityMonke.Patches
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Awake", MethodType.Normal)]
+    class PlayerPatch
+    {
+        private static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            /*
+            if(Behaviours.AirJump.instance == null)
+            {
+                __instance.gameObject.AddComponent<Behaviours.AirJump>();
+            }
+            */
+            __instance.gameObject.AddComponent<GravityMonke.WD.WristDisplay>();
+        }
+
+    }
+}
