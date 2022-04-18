74850e2e8cbc94ad3d6f2e8e2a0485bf4a1e6bfa
RaginggYT <100000931+RaginggYT@users.noreply.github.com>
2022-04-18 12:19:04 -0400

    Add files via upload

---
+++ GravityMonke/ComputerInterface/GravityEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ComputerInterface;
+using ComputerInterface.Interfaces;
+
+namespace GravityMonke.ComputerInterface
+{
+    // An IComputerModEntry basically just describes an entry in the main menu that you can select
+    public class GravityEntry : IComputerModEntry
+    {
+        // This is the mod name that is going to show up as a selectable mod
+        public string EntryName => "Gravity Monke";
+
+        // This is the first view that is going to be shown if the user select you mod
+        // The Computer Interface mod will instantiate your view 
+        public Type EntryViewType => typeof(GravityView);
+    }
+}
---
+++ GravityMonke/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GravityMonke
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.ragingg.gorillatag.gravitymonke";
+		public const string Name = "Gravity Monke";
+		public const string Version = "1.2.1";
+	}
+}
